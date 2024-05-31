e97ff3f14bc6d7d7d3d97e932df32947a758f330
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-05-31 10:54:09 -0500

    localStore use

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,51 @@ import { TodoItem } from './TodoItem.jsx';
 import { CreateTodoButton } from './CreateTodoButton.jsx';
 import React from 'react';
 
-const defaultTodos = [
-  {id: Math.floor(Math.random() * 1000000), text: 'Aprender MongoDB', completed: true},
-  {id: Math.floor(Math.random() * 1000000), text: 'Aprender GraphQL', completed: false},
-  {id: Math.floor(Math.random() * 1000000), text: 'Aprender React', completed: false},  
-  {id: Math.floor(Math.random() * 1000000), text: 'Aprender JavaScript', completed: true},
-]
+// const defaultTodos = [
+//   {id: Math.floor(Math.random() * 1000000), text: 'Aprender MongoDB', completed: true},
+//   {id: Math.floor(Math.random() * 1000000), text: 'Aprender GraphQL', completed: false},
+//   {id: Math.floor(Math.random() * 1000000), text: 'Aprender React', completed: false},  
+//   {id: Math.floor(Math.random() * 1000000), text: 'Aprender JavaScript', completed: true},
+// ]
+
+// localStorage.setItem('todos_v1', JSON.stringify(defaultTodos));
+// localStorage.removeItem('todos_v1');
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const localStoredTodos = localStorage.getItem('todos_v1');
+  
+  let parsedTodos;
+  if (!localStoredTodos) {
+    localStorage.setItem('todos_v1', JSON.stringify([]));
+  } else {
+    parsedTodos = JSON.parse(localStoredTodos);
+  }
+
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
   const searchValueFiltered = todos.filter(todo => todo.text.toLowerCase().includes(searchValue.toLowerCase()));
+  
+  const saveTodos = (newTodos) => {
+    localStorage.setItem('todos_v1', JSON.stringify(newTodos));
+    setTodos(newTodos)
+  };
   const competeTodo = (id) => {
     const newTodos = [...todos];
     const todoIndex = newTodos.findIndex(todo => todo.id === id);
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   }
   
   const deleteTodo = (id) => {
     const newTodos = [...todos];
     const todoIndex = newTodos.findIndex(todo => todo.id === id);
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   }
+
   console.log("Los valores ingresados son:",searchValue)
 
   return (
